0194199f51891e041b3a4cb26da6c717f4bd9636
psh1126 <psh1126@midasin.com>
2024-06-21 23:41:42 +0900

    Style: 이미지, 코드 블럭 margin border-radius 일부 스타일링

--- src/components/blog/PostMain.tsx
+++ src/components/blog/PostMain.tsx
@@ -4,6 +4,7 @@ import Giscus from '@/components/Giscus';
 import { Post } from '@/contentlayer/generated';
 import { Optional } from '@/types/common.type';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import './PostMain.scss';
 
 interface Props {
   post: Optional<Post>;
@@ -20,7 +21,9 @@ function PostMain({ post }: Props) {
       <p className={'description'}>설명: {post.excerpt}</p>
       <div className={'categories'}>카테고리: {post.categories}</div>
       <div className={'tags'}>태그: {post.tags}</div>
-      <div className={'read-time'}>읽는 데 걸리는 시간: {post.readingMinutes}</div>
+      <div className={'read-time'}>
+        읽는 데 걸리는 시간: {post.readingMinutes}
+      </div>
       <div className={'create-date'}>생성일시: {post.date}</div>
       <div className={'modified-date'}>수정일시: {post.last_modified_at}</div>
       <hr />
