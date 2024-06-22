c7e24c3234471eeb3a3563bc81f115df471ef6cb
psh1126 <psh1126@midasin.com>
2024-06-23 01:11:40 +0900

    Style: Divider space 및 className 추가

--- src/components/@common/Divider/Divider.tsx
+++ src/components/@common/Divider/Divider.tsx
@@ -6,23 +6,33 @@ const cx = classnames.bind(styles);
 export interface DividerProps {
   width?: string | 'full';
   height?: string | 'full';
+  space?: string;
   color?: string;
   style?: React.CSSProperties;
+  className?: string;
 }
 
 const Divider = ({
   width: _width = 'full',
-  height: _height = 'full',
+  height: _height = '1px',
+  space = '30px',
   color = COLORS.gray300,
   style,
+  className,
 }: DividerProps) => {
   const width = _width === 'full' ? '100%' : _width;
   const height = _height === 'full' ? '100%' : _height;
 
   return (
     <hr
-      className={cx('divider')}
-      style={{ ...style, width, height, backgroundColor: color }}
+      className={cx('divider', className)}
+      style={{
+        marginBlock: style?.marginBlock ?? space,
+        width,
+        height,
+        backgroundColor: color,
+        ...style,
+      }}
     />
   );
 };
