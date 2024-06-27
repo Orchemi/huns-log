ab1723e9f60a6fa51aa144c4d628e61f96cffc89
psh1126 <psh1126@midasin.com>
2024-06-27 19:26:52 +0900

    Feat: Header Logo 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,9 +1,22 @@
 import classnames from 'classnames/bind';
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './Header.module.scss';
 const cx = classnames.bind(styles);
 
 const Header = () => {
-  return <header className={cx('header-container')}></header>;
+  return (
+    <header className={cx('header-container')}>
+      <Link className={cx('logo')} href={'/'}>
+        <Image
+          src={'/assets/services/logo.svg'}
+          alt={'logo'}
+          width={100}
+          height={60}
+        />
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
